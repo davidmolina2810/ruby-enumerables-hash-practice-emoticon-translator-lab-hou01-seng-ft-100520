91f7d45833f68800621e53120e6c6861fe5bc3dc
David Molina <davidmolina2810@gmail.com>
2020-08-24 23:47:19 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -31,6 +31,7 @@ def get_english_meaning(file_path, emo)
   hash.each do |key, value|
     value.each do |inner_key, inner_value|
       if inner_value == emo
+        binding.pry 
         return hash[key][:english]
       end
     end
